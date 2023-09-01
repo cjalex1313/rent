33c2baff65da15461fe3701ab90646a42f869bb3
Alex Cioarec <cioarec.alexandru96@gmail.com>
2023-09-01 20:25:29 +0300

    Refactor reponse

---
+++ Rent.API/Models/Property/Apartment/ApartmentDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace Rent.API.Models.Property.Apartment;
+
+public class ApartmentDTO
+{
+    public int Id { get; set; }
+    public Guid OwnerId { get; set; }
+    public string Name { get; set; } = "";
+    public string Country { get; set; } = "";
+    public string City { get; set; } = "";
+    public string? State { get; set; }
+    public string Street { get; set; } = "";
+    public string Number { get; set; } = "";
+    public string PostalCode { get; set; } = "";
+    public double Size { get; set; }
+    public int Floor { get; set; }
+    public int BuildingMaxFloor { get; set; }
+    public int ApartmentNumber { get; set; }
+}
