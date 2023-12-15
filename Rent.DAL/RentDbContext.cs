7050fef7fbd000fb8908259a9f865e61b74525bf
Alex Cioarec <cioarec.alexandru96@gmail.com>
2023-12-15 02:08:14 +0200

    Add db entity for property images

--- Rent.DAL/RentDbContext.cs
+++ Rent.DAL/RentDbContext.cs
@@ -14,7 +14,7 @@ public class RentDbContext : IdentityDbContext<IdentityUser>
     public DbSet<UserDetail> UserDetails { get; set; }
     public RentDbContext(DbContextOptions<RentDbContext> options) : base(options)
     {
-        
+
     }
 
     protected override void OnModelCreating(ModelBuilder builder)
@@ -30,6 +30,7 @@ public class RentDbContext : IdentityDbContext<IdentityUser>
         builder.Entity<Property>().Property(p => p.Street).HasMaxLength(200);
         builder.Entity<Property>().Property(p => p.Number).HasMaxLength(20);
         builder.Entity<Property>().Property(p => p.PostalCode).HasMaxLength(20);
+        builder.Entity<Property>().HasOne(p => p.ThumnailImage).WithOne().HasForeignKey<Property>(p => p.ThumnailImageId);
 
         builder.Entity<Apartment>().ToTable("Apartments");
 
@@ -39,5 +40,10 @@ public class RentDbContext : IdentityDbContext<IdentityUser>
         builder.Entity<UserDetail>().Property(c => c.FirstName).HasMaxLength(70);
         builder.Entity<UserDetail>().Property(c => c.LastName).HasMaxLength(40);
         builder.Entity<UserDetail>().Property(c => c.AvatarExtension).HasMaxLength(10);
+
+        builder.Entity<PropertyImage>().ToTable("PropertyImages");
+        builder.Entity<PropertyImage>().HasKey(p => p.Id);
+        builder.Entity<PropertyImage>().Property(p => p.Extension).HasMaxLength(10);
+        builder.Entity<PropertyImage>().HasOne(p => p.Property).WithMany(p => p.Images).HasForeignKey(p => p.PropertyId);
     }
 }
